fa18ab1f370713748d6ecb75596f8929a6451122
Alex Le <nworld3d@yahoo.com>
2009-08-29 23:12:25 -0500

    - implement a new Priority Queue and unit test

--- javascripts/graph.js
+++ javascripts/graph.js
@@ -6,6 +6,9 @@ Graph.prototype = {
     
     this.graph = graph;
     this.name = 'Graph';
+    
+    this.HV_COST = 10;
+    this.D_COST  = 21;
 
     console.log( 'this.graph= %p', this.graph );
     
@@ -53,13 +56,14 @@ Graph.prototype = {
     {
       var node = openSet.pop();
       closedSet[ node.x + '_' + y ] = node;
-      var cost = 
+      //var cost = 
 
+      /*
       addNode( node.x + 1, node.y,        node.x, node.y );
       addNode( node.x    , node.y + 1,    node.x, node.y );
       addNode( node.x - 1, node.y,        node.x, node.y );
       addNode( node.x    , node.y - 1,    node.x, node.y );
-      
+      */
       // just incase ...
       if( i-- < 0 ){  
         console.log( 'exceed looping limit' );
@@ -88,10 +92,13 @@ Graph.prototype = {
       
       reconstruct reverse path from goal to start
       by following parent pointers
-
-
     */
 
+  }
+  
+  ,aStar1: function( startX, startY, endX, endY ) { 
+    
+    
   }
   
   ,isWalkable: function( x, y ) { 
@@ -103,227 +110,4 @@ Graph.prototype = {
 
 
 var Node = function( x, y) { this.x = x; this.y = y; };
-
-
-/* Shorted List */
-var Util = {};
-Util.SortedList = Class.create( { 
-	initialize: function( options ) {
-    options = Object.extend( { 
-      comparator: function(a,b) { return ( a < b ) ? -1 : ( a == b ) ? 0 : 1; }
-		  ,jsonifier: function(a) { return a.toJSON(); }
-    }, options || {} );
-
-		this.collection = [];
-		this.comparator = options.comparator;
-		this.jsonifier  = options.jsonifier;
-	}
-	
-	/* Return a new Sorted list with similar comparator and jsonifier */
-	,_clone: function() {
-	  return new Util.SortedList( { 
-	    comparator: this.comparator
-	    ,jsonifier: this.jsonifier
-    } );
-	}
-	
-	,push: function( item ) {
-		/* return the item and its index */
-		if( this.collection.length == 0 ) 
-		{
-			this.collection.push( item );
-			return 0;
-		}
-		/* special case: last item in the list */
-		if( this.comparator( this.collection[ this.collection.length - 1 ], item ) != 1 )
-		{ 
-			this.collection.push( item );
-			return this.collection.length - 1;
-		}
-		
-		/* special case:  first item in the list */
-		if( this.comparator( this.collection[0], item ) == 1 ) 
-		{
-			this.collection.splice( 0, 0, item );
-			return 0;
-		}
-		
-		/* searching for the item on the left, then splice the array accordingly */
-		var prevIndex = this.getPrevIndex( item );
-		this.collection.splice( prevIndex, 0, item );
-		
-		return prevIndex;
-	}
-	
-	,pop: function() { 
-	  return this.removeByIndex( 0 );
-	}
-	
-	,removeByIndex: function( i ) { 
-		if( i > this.count() || i < 0 ) { return null; }
-		return this.collection.splice( i, 1 )[0];
-	}
-	
-	,remove: function( caption ) {
-		var i = this.indexOf( caption );
-		if( i == -1 ) { return null; }
-		return this.collection.splice( i, 1 )[0];
-	}
-	
-	,clear: function() { this.collection = []; return this; }
-	
-	/* Perform a binary search to seek to the greatest smaller item */
-	,getPrevIndex: function( item ) {
-		var low = 0, high = this.collection.length, mid = 0;
-		do {
-			mid = Math.floor( ( low + high ) / 2 );
-			if( this.comparator( this.collection[ mid ], item ) < 1 )
-			{	low = mid + 1; }
-			else
-			{	high = mid; }
-		} while( low < high );
-		
-		return low--;
-	}
-	
-	,get: function( index ) {
-		return this.collection[ index ];
-	}
-
-  ,getByKey: function( item ) {
-		if( this.comparator( this.collection[ 0 ], item ) == 0 )	{	return this.collection[ 0 ]; }
-		else if( this.comparator( this.collection[ this.collection.length - 1 ], item ) == 0 )
-		  { return this.collection[ this.collection.length - 1]; }
-
-		var low = 0, high = this.collection.length, mid = 0;
-		do {
-			mid = Math.floor( ( low + high ) / 2 );
-			switch( this.comparator( this.collection[ mid ], item ) )
-			{
-			  case -1:
-			    low = mid + 1;
-			    break;
-			  case 0:
-			    return this.collection[ mid ];
-			  case 1:
-			    high = mid;
-			    break;
-		  }
-		} while( low < high );
-
-    return null;
-  }
-  
-		
-	,indexOf: function( item ) {
-		
-		for( var i = 0, len = this.collection.length; i < len; ++i )
-		{
-			if( this.comparator( this.collection[ i ], item ) == 0 )
-			{	return i; }
-		}
-		
-		return -1; // not found;
-	}
-
-	,each: function( iterator ) {
-		for( var i = 0, len = this.collection.length; i < len; ++i ) { 
-			if( typeof( temp = iterator( this.collection[i], i ) ) != 'undefined' && !temp )
-				break;
-		}
-	}
-	
-	/* Select all matching items using a boolean predicate */
-	,select: function( predicate ) {
-	  if( !predicate ) throw "a predicate is needed";
-	  
-	  var matches = this._clone();
-	  for( var i = 0, len = this.count(); i < len; i++ )
-	  {
-	    
-	    if( predicate( this.get( i ) ) )
-	      matches.push( this.get( i ) );
-	  }
-	  return matches;
-	}
-	
-	,first: function(){ return this.collection[0]; }
-	,last: function() { return this.collection[ this.collection.length - 1]; }
-	,count: function() { return this.collection.length; }	
-
-	/* Utility Method */
-	,toJSON: function() { 
-		var json = [];
-		for( var i = 0, len = this.collection.length; i < len; ++i )
-			json.push( this.jsonifier( this.collection[i] ) );
-		return "[" + json.join(",") + "]";
-	}
-
-	/* Uncomment this part to run Self-Unittest */
-	/*
-	,unitTest: function() {
-		this.collection = [];
-		this.push(5);
-		if( this.toString() != "[5]" ) 
-			throw "Failed. Expecting [5]";
-		
-		this.push(3);
-		if( this.toString() != "[3,5]" ) 
-			throw "Failed: Expecting [3,5]";
-	
-		this.push(4);
-		if( this.toString() != "[3,4,5]" ) 
-			throw "Failed: Expecting [3,4,5]";
-			
-		if( this.indexOf(4) != 1 )
-			throw "Failed. Expecting 1";
-		
-		if( this.indexOf(5) != 2 )
-			throw "Failed. Expecting 2";
-
-		if( this.indexOf(3) != 0 )
-			throw "Failed. Expecting 0";
-
-		if( this.indexOf(1) != -1 )
-			throw "Failed. Expecting -1";
-			
-		this.push(1);
-		if( this.toString() != "[1,3,4,5]" ) 
-			throw "Failed: Expecting [1,3,4,5]";
-		if( this.indexOf(1) != 0 )
-			throw "Failed. Expecting 0";
-		
-		this.push(8);
-		this.push(7);
-		if( this.toString() != "[1,3,4,5,7,8]" ) 
-			throw "Failed: Expecting [1,3,4,5,7,8]";
-		
-		// Get and Enumerable  tests
-		if ( this.get(0) != 1 )
-			throw "Failed: Expecting 1";
-
-		if ( this.get(1) != 3 )
-			throw "Failed: Expecting 3";
-
-		if ( this.last() != 8 )
-			throw "Failed: Expecting 8";
-	
-		if ( this.first() != 1 )
-			throw "Failed: Expecting 1";
-
-		// Remove
-		if( this.remove(8) != 8 || this.count() != 5 )
-			throw "Failed: Expecting 8 and items count of 5";
-		if( !this.last() == 7 || this.first() != 1 )
-			throw "Failed: Expecting 7 and 1";
-		if( this.removeByIndex(4) != 7 ) { 
-			throw "Failed: Expecting 7, items count of 4, and 5 as the last item";
-		}
-		this.collection = [];
-	} 
-	*/
-	
-	,toString: function() { return "[" + this.collection.join(",") + "]"; }
-} );
-
 
---
+++ javascripts/utilities.js
@@ -0,0 +1,68 @@
+
+/* Shorted List */
+var Util = {};
+
+Util.PQ = Class.create( { 
+initialize: function( options ) {
+		this.collection = [];
+		this.values = [];
+	}
+	
+	,push: function( item, value ) {
+		/* return the item and its index */
+		if( this.collection.length == 0 ) 
+		{
+			this.collection.push( item );
+			this.values.push( value );
+			return;
+		}
+
+		/* special case: last item in the list */
+		if( this.values[ this.values.length - 1 ] < value )
+		{ 
+			this.collection.push( item );
+			this.values.push( value );
+			return;
+		}
+		
+		/* special case:  first item in the list */
+		if( this.values[ 0 ] > value ) 
+		{
+			this.collection.splice( 0, 0, item );
+			this.values.splice( 0, 0, value );
+			return;
+		}
+		
+		/* searching for the item on the left, then splice the array accordingly */
+		var prevIndex = this.getPrevIndex( value );
+		this.collection.splice( prevIndex, 0, item );
+		this.values.splice( prevIndex, 0, value );
+	}
+	
+	/* Perform a binary search to seek to the greatest smaller item */
+	,getPrevIndex: function( value ) {
+		var low = 0, high = this.collection.length, mid = 0;
+		do {
+			mid = Math.floor( ( low + high ) / 2 );
+			if(  this.values[ mid ] < value  )
+			{	low = mid + 1; }
+			else
+			{	high = mid; }
+		} while( low < high );
+		return low--;
+	}
+		
+	,pop: function() { 
+	  return this.removeByIndex( 0 );
+	}
+	
+	,removeByIndex: function( i ) { 
+		if( i > this.count() || i < 0 ) { return null; }
+		return [ this.collection.splice( i, 1 )[0], this.values.splice( i, 1 )[0] ];
+	}
+  
+	,count: function() { return this.collection.length; }	
+	
+	,clear: function() { this.collection = []; this.values = []; return this; }	
+	
+} );
